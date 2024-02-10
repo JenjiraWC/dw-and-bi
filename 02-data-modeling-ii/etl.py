b2ba685701463c8f0e9df37982f87b579365abca
JenjiraWC <157778580+JenjiraWC@users.noreply.github.com>
2024-02-10 09:48:01 +0000

    edit code

--- 02-data-modeling-ii/etl.py
+++ 02-data-modeling-ii/etl.py
@@ -80,34 +80,6 @@ def process(session, filepath):
                 # Insert data into tables here
 
 
-def insert_data(data,session):
-    query = """
-    INSERT INTO events 
-    (
-        id,
-        type,
-        actor_id,
-        repo_id,
-        payload,
-        public,
-        created_at,
-        org_id
-    )
-    VALUES 
-    (
-        '{data["id"]}',
-        '{data["type"]}', 
-        '{data["actor"]["id"]'},
-        '{data["repo"]["id"]}',
-        '{data["payload"]["action"]}',
-        '{data["public"]}',
-        '{data["created_at"]}',
-        '{data["org"]["id"]}'
-    )
-    """
-
-    session.execute(query)
-
 def insert_sample_data(session):
     query = """
     INSERT INTO events (id, type, actor_id, repo_id, payload, public, created_at, org)
